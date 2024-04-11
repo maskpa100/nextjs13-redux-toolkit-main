19051f8c688a972b6886ab3bcbf9f230e9c36395
maskpa100 <maskpa@yandex.ru>
2024-04-11 23:13:00 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Home from "./www";
+
+export default async function Page() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
---
+++ src/app/www.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useGetUsersQuery } from "@/redux/services/userApi";
+import { decrement, increment, reset } from "@/redux/features/counterSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { useState } from "react";
+
+export default function Home() {
+  const count = useAppSelector((state) => state.counterReducer.value);
+  const dispatch = useAppDispatch();
+  const [w, setW] = useState(0);
+  if (w === 0) {
+    dispatch(reset());
+    dispatch(increment());
+    setW(1);
+  }
+  return (
+    <main style={{ maxWidth: 1200, marginInline: "auto", padding: 20 }}>
+      <div style={{ marginBottom: "4rem", textAlign: "center" }}>
+        <h4 style={{ marginBottom: 16 }}>{count}</h4>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button
+          onClick={() => dispatch(decrement())}
+          style={{ marginInline: 16 }}>
+          decrement
+        </button>
+        <button onClick={() => dispatch(reset())}>reset</button>
+      </div>
+    </main>
+  );
+}
